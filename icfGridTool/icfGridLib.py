8a81d762659426df9d1a39467fcea32c410c51b7
Alex Sparrow <alex@alex-laptop.(none)>
2010-02-21 20:51:00 +0100

    * New version

---
+++ icfGridTool/icfGridLib.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+from types import *
+import os,os.path
+import ConfigParser
+
+global_defaults={
+    "jobdir":(StringType,"./jobs","Job storage directory"),
+    "email":(StringType,"","Email address for CrabServer"),
+}
+
+job_defaults={
+    "name":(StringType,"","Name of job"),
+    "sample":(StringType,"","Sample dataset path"),
+    "script":(StringType,"icfNtuple_cfg.py","Path to CMSSW script"),
+    "globaltag":(StringType,"MC_3XY_V18::All","Global Tag to use"),
+    "crabtemplate":(StringType,"./crab_tmpl.cfg","Path to CRAB template script"),
+    "eventsperjob":(IntType,10000,"Number of events per job."),
+    "events":(IntType,-1,"Total number of events to process."),
+    "mcinfo":(BooleanType,True,"Flag to generate MC info."),
+    "scheduler":(StringType,"glite_slc5","CRAB Scheduler."),
+    "servername":(StringType,"legnaro","CrabServer name."),
+    "copydata":(BooleanType,True,"Crab copy data to storage."),
+    "returndata":(BooleanType,False,"Crab return data to user."),
+    "storagepath":(StringType,"/castor/cern.ch","Crab storage path."),
+    "userremotedir":(StringType,"","Crab user remote directory."),
+    "storageelement":(StringType,"srm-cms.cern.ch","Crab storage element."),
+    "status":(StringType,"CREATED","Job status")
+}
+
+def myInput(vtype,name):
+    cool=False
+    val=None
+    while not cool:
+        tmp=raw_input(name)
+        if vtype==StringType:
+            val=tmp
+            cool=True
+        elif vtype==IntType:
+            try:
+                val=int(tmp)
+                cool=True
+            except ValueError:
+                print "Incorrect value for int: %s" % tmp
+        elif vtype==BooleanType:
+            try:
+                val=int(tmp)
+                if val<0 or val >1:
+                    raise ValueError
+                else:
+                    val=bool(val)
+                cool=True
+            except ValueError:
+                print "Incorrect value for bool: %s" % tmp
+        if not cool:
+            print "Please try again."
+    return val
+
+def getRaw(val,vtype):
+    if vtype==BooleanType:
+        if val:
+            return 1
+        else:
+            return 0
+    else:
+        return val
+
+class CrabJob:
+    def __init__(self,job,glob,path):
+        f=open(job.get("crabtemplate"),"r")
+        crab=f.read()
+        for (k,v) in job.params.iteritems():
+            crab=crab.replace("__"+k+"__",str(getRaw(v[1],v[0])))
+        for(k,v) in glob.iteritems():
+            crab=crab.replace("__"+k+"__",str(getRaw(v[1],v[0])))
+        f=open(path+"/crab.cfg","w+")
+        f.write(crab)
+
+class Job:
+    def __init__(self,params):
+        self.params=params
+
+    def get(self,name):
+        if not name in self.params:
+            raise KeyError("Parameter not found: %s" % name)
+        return self.params[name][1]
+
+    def _set(self,name,value):
+        self.params[name]=(self.params[name][0],value,self.params[name][2])
+
+    def set(self,name,value):
+        if not name in self.params:
+            raise KeyError("Paramater not found: %s" % name)
+        if type(value)==self.params[name][0]:
+            self._set(name,value)
+        elif type(value)=="str":
+            if self.params[name][0]=="int":
+                self._set(name,int(value))
+            elif self.params[name][0]=="bool":
+                if value=="1":
+                    self._set(name,True)
+                elif value=="0":
+                    self.set(name,False)
+                else:
+                    msg="Invalid literal for boolean %s ('%s')" % (name, value)
+                    raise ValueError(msg)
+        else:
+            print str(type(value))
+            print self.params[name][0]
+            msg="Invalid literal for %s (%s) : '%s'" %(name,
+                                                       self.params[name][0],
+                                                       value)
+            raise ValueError(msg)
+
+    def __repr__(self):
+        strs=[]
+        for (k,v) in self.params.iteritems():
+            strs.append("%s : %s" % (k,v[1]))
+            strs.append("\n")
+        del strs[len(strs)-1]
+        return "".join(strs)
+
+    def crabCreate(self,glob,path):
+        if(os.path.exists(path)):
+            raise IOError("Path %s already exists" % path)
+        os.mkdir(path)
+        c=CrabJob(self,glob,path)
+        self.crab_job=c
+
+    def crabSubmit(self):
+        self.crab_job.submit()
+
+class Config:
+    def __init__(self,fname):
+        self.conf=ConfigParser.RawConfigParser()
+        self.conf.read(fname)
+        self.job_defaults=job_defaults
+        self.globals=global_defaults
+        self.fname=fname
+
+    @staticmethod
+    def initDefault(fname):
+        conf=ConfigParser.RawConfigParser()
+        Config.writeSection(conf,job_defaults,"DEFAULT")
+        Config.writeSection(conf,global_defaults,"DEFAULT")
+        with open(fname, 'wb') as configfile:
+            conf.write(configfile)
+
+    @staticmethod
+    def readSection(conf,params,secname):
+        for (k,v) in params.iteritems():
+            if v[0]==StringType:
+                val=conf.get(secname,k)
+            elif v[0]==IntType:
+                val=conf.getint(secname,k)
+            elif v[0]==BooleanType:
+                val=False
+                tmp=conf.getint(secname,k)
+                if(tmp==1):
+                    val=True
+                elif(tmp==0):
+                    val=False
+                else:
+                    raise ValueError
+            params[k]=(params[k][0],val,params[k][2])
+
+    @staticmethod
+    def writeSection(conf,params,secname):
+        for (k,v) in params.iteritems():
+            conf.set(secname,k,getRaw(v[1],v[0]))
+
+    def write(self):
+        with open(self.fname, 'wb') as configfile:
+            self.conf.write(configfile)
+
+    def readGlobals(self):
+        self.readSection(self.conf,self.job_defaults,"DEFAULT")
+        self.readSection(self.conf,self.globals,"DEFAULT")
+
+    def writeGlobals(self):
+        self.writeSection(self.conf,self.job_defaults,"DEFAULT")
+        self.writeSection(self.conf,self.globals,"DEFAULT")
+
+    def readJobs(self):
+        jobs={}
+        for s in self.conf.sections():
+            job_params=self.job_defaults.copy()
+            self.readSection(job_params,s)
+            job_name=job_params["name"][1]
+            if job_name in jobs:
+                raise KeyError("Duplicate job names detected")
+            jobs[job_name]=Job(job_params)
+        return jobs
+
+    def writeJobs(self,jobs):
+        for (k,v) in jobs.iteritems():
+            if not self.conf.has_section(k):
+                self.conf.add_section(k)
+            self.writeSection(self.conf,v.params,k)
+        self.write()
+
+    def printGlobals(self):
+        for (k,v) in self.job_defaults.iteritems():
+            print "%s = %s" % (k,v[1])
+        for (k,v) in self.globals.iteritems():
+            print "%s = %s" % (k,v[1])
+
+if __name__=="__main__":
+    pass
--- icfGridTool/icfGridTool.py
+++ icfGridTool/icfGridTool.py
@@ -1,82 +1,101 @@
 #!/usr/bin/env python
+import cmd,readline,os,os.path
+from icfGridLib import *
 
-import cmd,sys,readline,os,os.path
-
-class CrabJob:
-    def __init__(self,job):
-        self.job=job
-
-    def create(self):
-        pass
-
-class Job:
-    def __init__(self,name,sample):
-        self.name=name
-        self.sample=sample
-
-    def setup_dir(self,path):
-        if not os.path.exists(path):
-            os.mkdir(path)
-        else:
-            raise Exception("Directory already exists")
-        self.path=path
+header_eq="="*50
 
 class ICFGridTool(cmd.Cmd):
-    def __init__(self):
+    def __init__(self,config="./icf.cfg"):
         cmd.Cmd.__init__(self)
         self.prompt="icf> "
         self.jobs={}
-        self.header_eq="========================================"
-        self.job_dir="./jobs"
+        if not os.path.exists(config):
+            print "No file icf.cfg found"
+            print "Generating defaults."
+            Config.initDefault(config)
+        self.config=Config(config)
+        print "Config:"
+        print header_eq
+        self.config.printGlobals()
+        print header_eq
 
-    def do_create(self,name="",sample=""):
-        if name == "":
-            name = raw_input("Enter job name: ")
-        if sample == "":
-            sample=raw_input("Enter sample path: ")
-        job=Job(name,sample)
-        try:
-            job.setup_dir(self.job_dir+"/"+name)
-        except Exception as e:
-            print e
+    def emptyline(self):
+        pass
+
+    def do_create(self,name=""):
+        print "Create Job:"
+        name=myInput(StringType,"Name:")
+        if name in self.jobs:
+            print "ERROR: Job already exists with name: %s" %name
             return
-        self.jobs[name]=job
-        print "Job %s created in directory %s" % (name,job.path)
+        sample=myInput(StringType,"Dataset Path:")
+        mcinfo=myInput(BooleanType,"Generate MC:")
+        j=Job(self.config.job_defaults)
+        print name
+        j.set("name",name)
+        j.set("sample",sample)
+        j.set("mcinfo",mcinfo)
+        self.jobs[name]=j
 
     def help_create(self):
-        print "Creates a grid job"
+        print "Create a job"
+
+    def do_edit(self,name):
+        if not name in self.jobs:
+            print "ERROR: Unknown job: %s" % name
+            return
+        print "Edit Job:"
+        param=myInput(StringType,"Parameter:")
+        if not param in self.jobs[name].params:
+            print "Unknown paramater: %s" % param
+            return
+        value=myInput(self.jobs[name].params[param][0],
+                      "New value (previous=%s):" % self.jobs[name].get(param))
+        self.jobs[name].set(param,value)
+
+    def help_edit(self,name):
+        print "Edit a job"
+
+    def do_show(self,name):
+        if not name in self.jobs:
+            print "ERROR: Unknown job: %s" % name
+            return
+        print header_eq
+        print "Job : %s" % name
+        print "Status : %s "%self.jobs[name].get("status")
+        print header_eq
+        print self.jobs[name]
+
+    def help_show(self):
+        print "Show an individual job"
+
+    def do_crab_create(self,name):
+        if not name in self.jobs:
+            print "ERROR: Unknown job: %s" % name
+            return
+        self.jobs[name].crabCreate(self.config.globals,
+                                   self.config.globals["jobdir"][1]+
+                                   "/"+name)
 
     def do_list(self,name=""):
         print "Jobs:"
-        print self.header_eq
+        print header_eq
         if not len(self.jobs):
             print "No jobs yet!"
         for (name,j) in self.jobs.iteritems():
-            print "%s\t%s" % (name,j.sample)
-        print self.header_eq
+            print "%s\t%s\t%s" % (j.get("name"),j.get("sample"),j.get("status"))
+        print header_eq
 
     def help_list(self):
         print "List jobs"
 
-    def do_crab_create(self,name):
-        if not name in self.jobs:
-            print "ERROR: No such job"
-            return
-        j=self.jobs[name]
-        crab=CrabJob(j)
-        j.crab=crab
-        try:
-            crab.create()
-        except Exception as e:
-            print e
-
-
-    def help_EOF(self):
-        print "Quits the program"
 
     def do_EOF(self, line):
         sys.exit()
 
+    def help_EOF(self):
+        print "Quits the program"
+
 if __name__=="__main__":
-    icf=ICFGridTool()
-    icf.cmdloop()
+    tool=ICFGridTool()
+    tool.cmdloop()
